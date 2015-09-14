c0daea821b846f428354e70e2112312454a8080a
definedd4 <definedd4@gmail.com>
2015-09-14 19:43:37 +0000

    Modified test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world!\n";
+    int a, b;
+    std::cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << "a + b = " << a + b << endl;
     return 0;
 }
